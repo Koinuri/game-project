ee40d215eec881d64c06b26665832f545a8a5cd8
Koinuri <koinuri373dreams@gmail.com>
2017-06-17 16:38:47 -0500

    Cleaned the main.go so that it has as little OpenGL codes as possible

---
+++ main/framework/painter.go
@@ -0,0 +1,131 @@
+package framework
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/go-gl/gl/v4.5-core/gl"
+	"github.com/go-gl/glfw/v3.2/glfw"
+)
+
+const (
+	vertexShaderSource = `
+        #version 450
+        in vec3 vp;
+		in vec2 tx;
+
+		out vec2 TexCoord;
+
+        void main() {
+            gl_Position = vec4(vp, 1.0);
+			TexCoord = tx;
+        }
+    ` + "\x00"
+	fragmentShaderSource = `
+        #version 450
+		in vec2 TexCoord;
+
+		out vec4 frag_colour;
+		
+		uniform sampler2D ourTexture;
+
+        void main() {
+            frag_colour = texture(ourTexture, TexCoord);
+        }
+    ` + "\x00"
+)
+
+func Init(width int, height int) (*glfw.Window, uint32) {
+	window := initGlfw(width, height)
+	prog := initOpenGL()
+	return window, prog
+}
+
+func initGlfw(width int, height int) *glfw.Window {
+	if err := glfw.Init(); err != nil {
+		panic(err)
+	}
+
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 5)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	window, err := glfw.CreateWindow(width, height, "Game Project", nil, nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+
+	return window
+}
+
+func initOpenGL() uint32 {
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+	log.Println("OpenGL version", version)
+
+	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		panic(err)
+	}
+
+	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		panic(err)
+	}
+
+	prog := gl.CreateProgram()
+	gl.AttachShader(prog, vertexShader)
+	gl.AttachShader(prog, fragmentShader)
+	gl.LinkProgram(prog)
+	return prog
+}
+
+func Draw(obj Artist, window *glfw.Window, prog uint32) {
+	vao, texture := obj.GetDrawInfo()
+	gl.ClearColor(0.1, 0.2, 0.3, 1.0)
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+	gl.UseProgram(prog)
+
+	gl.BindTexture(gl.TEXTURE_2D, texture)
+	gl.BindVertexArray(vao)
+	gl.DrawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, gl.PtrOffset(0))
+
+	glfw.PollEvents()
+	window.SwapBuffers()
+}
+
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
+}
+
+func Clean() {
+	glfw.Terminate()
+}
